36e20bf844879cc092e6c154b705863515f6dfb0
Ashwin Ram <ashwin.ram0308@gmail.com>
2023-05-07 16:02:58 -0500

    Update the file to take arguments rather than manual changes in code

--- utils/markdown_converter.py
+++ utils/markdown_converter.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-# TODO replace
-#TODO add args
+
+import argparse
 import nbformat
 from nbconvert.exporters import MarkdownExporter, NotebookExporter
 
-# Define the input and output file paths
-input_file = 'input.ipynb'
-output_file = 'output.md'
+# Define command line arguments
+parser = argparse.ArgumentParser(description='Convert Jupyter notebooks and Markdown files.')
+parser.add_argument('input_file', help='path to the input file')
+parser.add_argument('output_file', help='path to the output file')
+args = parser.parse_args()
 
 # Create a notebook exporter
 notebook_exporter = NotebookExporter()
@@ -15,28 +17,28 @@ notebook_exporter = NotebookExporter()
 markdown_exporter = MarkdownExporter()
 
 # Check if the input file is a notebook or a markdown file
-if input_file.endswith('.ipynb'):
+if args.input_file.endswith('.ipynb'):
     # Read the input notebook file
-    with open(input_file, 'r') as f:
+    with open(args.input_file, 'r') as f:
         nb = nbformat.read(f, as_version=4)
     
     # Export the notebook to markdown
     (markdown, _) = markdown_exporter.from_notebook_node(nb)
     
     # Write the markdown to the output file
-    with open(output_file, 'w') as f:
+    with open(args.output_file, 'w') as f:
         f.write(markdown)
     
-elif input_file.endswith('.md'):
+elif args.input_file.endswith('.md'):
     # Read the input markdown file
-    with open(input_file, 'r') as f:
+    with open(args.input_file, 'r') as f:
         markdown_text = f.read()
     
     # Export the markdown to a notebook
-    (nb, _) = notebook_exporter.from_filename(input_file)
+    (nb, _) = notebook_exporter.from_filename(args.input_file)
     
     # Write the notebook to the output file
-    nbformat.write(nb, output_file)
+    nbformat.write(nb, args.output_file)
     
 else:
-    print(f"Error: unsupported file type for input file '{input_file}'.")
+    print(f"Error: unsupported file type for input file '{args.input_file}'.")
