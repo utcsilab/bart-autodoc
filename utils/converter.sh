2e79c883db23d129f550111d228c7ad9fdb69ee0
Ashwin Ram <ashwin.ram0308@gmail.com>
2023-05-19 00:15:05 -0500

    Completely fixed the master script
    The master script is now able to convert FROM an MD TO a IPYNB while PRESERVING the STRUCTURE.

--- utils/converter.sh
+++ utils/converter.sh
@@ -1,39 +1,18 @@
-#Bash script that can handle the conversion between Markdown and Jupyter notebooks (.ipynb) based on user input:
- 
-
 #!/bin/bash
 
-# Define the input and output file paths
-input_file="$1"
-output_file="$2"
+# Check if the required command is available
+command -v jupytext >/dev/null 2>&1 || { echo >&2 "jupytext is required but not installed. Aborting."; exit 1; }
 
-# Check if input and output files are provided
-if [ -z "$input_file" ] || [ -z "$output_file" ]; then
-    echo "Error: Input and output files are required."
-    echo "Usage: ./convert.sh <input_file> <output_file>"
-    exit 1
+# Check if a file argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <markdown-file>"
+  exit 1
 fi
 
-# Check if the input file exists
-if [ ! -f "$input_file" ]; then
-    echo "Error: Input file '$input_file' does not exist."
-    exit 1
-fi
+markdown_file="$1"
+notebook_file="${markdown_file%.*}.ipynb"
 
-# Check if the input file is a Markdown file
-if [[ "$input_file" == *.md ]]; then
-    # Convert Markdown to Jupyter notebook
-    jupyter nbconvert --to notebook --output-dir=./ --output="$output_file" "$input_file"
-    # Convert Jupyter notebook to Markdown
-    jupyter nbconvert --to markdown --output-dir=./ --output="${output_file%.*}.md" "$output_file"
-elif [[ "$input_file" == *.ipynb ]]; then
-    # Convert Jupyter notebook to Markdown
-    jupyter nbconvert --to markdown --output-dir=./ --output="$output_file" "$input_file"
-    # Convert Markdown to Jupyter notebook
-    jupyter nbconvert --to notebook --output-dir=./ --output="${output_file%.*}.ipynb" "$output_file"
-else
-    echo "Error: Unsupported file type for input file '$input_file'."
-    exit 1
-fi
+# Convert Markdown to Jupyter Notebook format using jupytext
+jupytext --to notebook "$markdown_file" --output "$notebook_file"
 
-echo "Conversion complete: $input_file converted to $output_file."
+echo "Conversion complete: $markdown_file converted to $notebook_file."
