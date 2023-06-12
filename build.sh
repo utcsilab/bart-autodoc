23cdab4dcfe676a29c6aada229f65d22d9b30ed4
Jon Tamir <jtamir@utexas.edu>
2023-06-12 14:18:18 -0500

    make build script more verbose

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # 1. convert files that first need to be converted to ipynb
+echo "Converting notebook MD files to ipynb format:"
 cat notebook_files.lst | xargs -I {} ./utils/convert.sh {}.md
 cat notebook_files.lst | xargs -I {} mv {}.ipynb doc/manual/
+echo "...Done!"
 
+echo "Building manual:"
 jupyter-book build  doc/manual
+echo "...Done!"
