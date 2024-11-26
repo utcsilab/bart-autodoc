92e1d523164f71446ed90f74c58f76d63a59c08c
Jon Tamir <jtamir@utexas.edu>
2024-11-26 13:23:06 -0600

    restructure directories

--- build.sh
+++ build.sh
@@ -2,15 +2,19 @@
 
 echo "Converting notebook MD files to ipynb format:"
 cat notebook_files.lst | xargs -I {} ./utils/convert.sh {}.md
-cat notebook_files.lst | xargs -I {} mv {}.ipynb doc/manual/
+cat notebook_files.lst | xargs -I {} mv {}.ipynb manual/
 echo "...Done!"
 
 echo "Building manual:"
-#jupyter-book clean doc/manual
-jupyter-book build  doc/manual
+#jupyter-book clean manual
+jupyter-book build  manual
 echo "...Done!"
 
 echo "Remove temporary bart files:"
-rm -f doc/manual/*.cfl
-rm -f doc/manual/*.hdr
+rm -f manual/*.cfl
+rm -f manual/*.hdr
+echo "...Done!"
+
+echo "Sync html pages:"
+rsync -av manual/_build/html/* doc/
 echo "...Done!"
